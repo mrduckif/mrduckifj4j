08465a0fd078c8eda54bdfe420dcbfa1e8d92c06
mrduckif <63663204+mrduckif@users.noreply.github.com>
2020-09-19 14:42:40 +0200

    Create oyuncularsehri.js

---
+++ oyuncularsehri.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Oyuncular Şehri | J4J BOT");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+
+client.on('ready', () => {
+        console.log(`${client.user.username} ismi ile giriş yapıldı! ${client.guilds.size} Sunucu, ${client.users.size} Kullanıcı.`);
+});
+
+client.on("message", async msg => {
+  if (msg.channel.type === "dm") {
+    if (msg.author.id === client.user.id) {
+    } else {
+      if (msg.author.bot) {
+      } else {
+        let oyuncularsehri = await data.get(msg.author.id)
+        
+        if (oyuncularsehri === 1) {
+        } else {
+          await data.set(msg.author.id, 1)
+          await sleep(1500)
+          await msg.channel.send("J4J \n https://discord.gg/A49JACK") // Discord sunucunuzun kalıcı linkini girin size mesaj atanlara oto dönüş yapması için.      
+          await sleep(1800)
+          await msg.channel.send("Done")
+          await sleep(1900)
+          await msg.channel.send("Leave=Leave") 
+      }
+      }
+    }
+  }
+})
+
+
+client.on("guildCreate", oyuncularsehri => {
+  sleep(1000);
+  if (
+    oyuncularsehri.id === "643154982996279323" ||  // j4j sunucusunun idsi
+    oyuncularsehri.id === "643154982996279323"  // j4j sunucusunun idsi
+  ) {
+    
+  } else {
+    oyuncularsehri.leave();
+  }
+  });
+
+client.on("ready", () => {
+      setInterval(() => {
+       let a = client.channels.get("756508485084119111") // j4j sunucusunun kanal id'si
+
+
+       a.send("J4J DM")
+
+
+      }, 30000);
+})
+
+client.on("ready", () => {
+      setInterval(() => {
+       let a = client.channels.get("756508485084119111") // j4j sunucusunun kanal id'si
+
+
+       a.send("j4j DM me")
+
+
+      }, 60000);
+})
+
+client.login("token") // user tokeniniz
